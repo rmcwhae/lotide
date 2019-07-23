5a09bb591f62c6be3b5cb21d7b1c0de7f0986a90
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-23 21:04:13 +0000

    Working eqArrays compare function

--- eqArrays.js
+++ eqArrays.js
@@ -6,3 +6,19 @@ const assertEqual = function(actual, expected, message) {
     console.log(`🛑🛑🛑 Assertion Failed`);
   }
 };
+let eqArrays = function(inputArray1, inputArray2) {
+  if (inputArray1.length !== inputArray2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < inputArray1.length; i++) {
+      // console.log(`arr1: ${inputArray1[i]}; arr2: ${inputArray2[i]}`);
+      if (inputArray1[i] !== inputArray2[i]) return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
