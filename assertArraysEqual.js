646ee6aaa39ae71ec6d81483c5136977eccbe776
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-23 21:10:36 +0000

    Adds assertArraysEqual assertion function

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+// Assertion Function
+
+let assertArraysEqual = function(inputArray1, inputArray2, message) {
+  if (eqArrays(inputArray1, inputArray2)) {
+    console.log(`✅✅✅ Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  }
+}
+
+
+let eqArrays = function(inputArray1, inputArray2) {
+  if (inputArray1.length !== inputArray2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < inputArray1.length; i++) {
+      // console.log(`arr1: ${inputArray1[i]}; arr2: ${inputArray2[i]}`);
+      if (inputArray1[i] !== inputArray2[i]) return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
