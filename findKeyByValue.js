4fc6090721f9bc04520d13ce08f5c3d59b9956d0
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-24 15:37:53 +0000

    Working solution to findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,13 +7,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKeyByValue = function(obj, val) {
-
+// givenValue: a string to search for in the values of the givenObject
+// givenObject: object containing keys and values
+const findKeyByValue = function(givenObject, givenValue) {
+  let output = undefined;
+  for (let item in givenObject) {
+    if (givenObject[item] === givenValue) output = item;//return key if givenValue matches selectedObject value
+  }
+  return output;
 };
 
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  'sci_fi': "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
