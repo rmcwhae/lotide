94c71e7e8814fb482e71f87fef856e706cfeade0
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-24 15:40:34 +0000

    Adds eqObjects function skeleton

--- eqObjects.js
+++ eqObjects.js
@@ -6,3 +6,8 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
