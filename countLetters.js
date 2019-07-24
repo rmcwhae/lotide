8accb4aec9fc1be5c56e55c48c21c223f92d1a8d
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-24 13:17:31 +0000

    Adds working countLetters function:

--- countLetters.js
+++ countLetters.js
@@ -7,8 +7,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function () {
+const countLetters = function (inputString) {
   const results = {};
-
+  inputString = inputString.split(" ").join("");//remove spaces from input
+  for (const letter of inputString) {
+    if (results[letter]) results[letter] += 1;//if letter already exists increase count
+    else results[letter] = 1; //if letter doesn't alredy exist add it
+  }
   return results;
 };
+
+console.log(countLetters("hello"));
