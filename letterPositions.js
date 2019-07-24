67aefa01d6d1b94911def5623e5e6bf9cd973162
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-24 13:47:41 +0000

    Working letterPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -9,20 +9,29 @@ let assertArraysEqual = function(inputArray1, inputArray2, message) {
 
 
 let eqArrays = function(inputArray1, inputArray2) {
+  let equivalent = true;
   if (inputArray1.length !== inputArray2.length) {
-    return false;
+    equivalent = false;
   } else {
     for (let i = 0; i < inputArray1.length; i++) {
-      // console.log(`arr1: ${inputArray1[i]}; arr2: ${inputArray2[i]}`);
-      if (inputArray1[i] !== inputArray2[i]) return false;
+      console.log(`arr1: ${inputArray1[i]}; arr2: ${inputArray2[i]}`);
+      if (inputArray1[i] !== inputArray2[i]) equivalent = false;
     }
   }
-  return true;
+  return equivalent;
 };
 
 
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+  sentence = sentence.split(" ").join("");//remove spaces from input
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) results[sentence[i]].push(i);//for each letter, add it's index to the results array for that letter's key
+    else results[sentence[i]] = [i];
+  }
+  // console.log(results;)
   return results;
 };
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
