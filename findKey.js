76f3ce3fc3576c2784c3ae6a7594de6c044ffd0f
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-25 17:58:20 +0000

    Adds working finkKey function

--- findKey.js
+++ findKey.js
@@ -6,3 +6,36 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    console.log('stars is', object[item].stars);
+    // console.log('callback of item is', callback(item));
+    // console.log('item is', item);
+    if (callback(object[item])) {//if callback becomes true, return the index of where it happened
+      return item;
+    }
+  }
+};
+
+let results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(results1, "noma");
+
+let results2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+assertEqual(results2, "Akaleri");
