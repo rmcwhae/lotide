e63492bfe5ffc1434e6c5e3bd76507e6030a7c3c
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-25 16:13:09 +0000

    Adds working takeUntil solution

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,4 @@
-// Assertion Function
-
+// Assertion Functions
 let assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {//use eqArrays function to compare arrays
     console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);//log appropriate message
@@ -25,5 +24,24 @@ let eqArrays = function(inputArray1, inputArray2) {
 
 //return a slice of the array with elements taken from the beginning until the callback returns a truthy value
 const takeUntil = function(array, callback) {
-  // ...
+  let output = [];
+  for (let item of array) {
+    // console.log('item is: ', item);
+    // console.log('callback of item is: ', callback(item));
+    if (callback(item)) {
+      break;//if callback(item) becomes true, exit the loop
+    } else {
+      output.push(item);//otherwise keep adding to the loop
+    }
+  }
+  return output;
 };
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
