a42dd94d55ce09f407d81771128593c93fabaf3a
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-26 21:07:28 +0000

    FIxed linting error

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -51,7 +51,7 @@ const dc = { d: ["2", 3], c: "1" };
 assertObjectsEqual(cd, dc);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertObjectsEqual(cd, cd2); //false
+assertObjectsEqual(cd, cd2); //false
 
 
 
