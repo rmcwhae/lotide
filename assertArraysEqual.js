37fbdc9f1ff2764c85ecc1daf2a038b2071d117f
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-26 21:51:31 +0000

    Recursive eqArrays function in assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,4 @@
 // Assertion Function
-
 let assertArraysEqual = function(actual, expected, message) {
   if (eqArrays(actual, expected)) {//use eqArrays function to compare arrays
     console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);//log appropriate message
@@ -8,17 +7,28 @@ let assertArraysEqual = function(actual, expected, message) {
   }
 };
 
-
 let eqArrays = function(inputArray1, inputArray2) {
+  let arraysEquivalent = true;
   if (inputArray1.length !== inputArray2.length) {
-    return false;
+    arraysEquivalent = false;
   } else {
     for (let i = 0; i < inputArray1.length; i++) {
-      // console.log(`arr1: ${inputArray1[i]}; arr2: ${inputArray2[i]}`);
-      if (inputArray1[i] !== inputArray2[i]) return false;
+      //if item is an array, call func recursively
+      if (Array.isArray(inputArray1[i])) {
+        arraysEquivalent = (eqArrays(inputArray1[i], inputArray2[i]));
+      } else {//continue only if item is not a sub-array (base case)
+        for (let j = 0; j < inputArray1.length; j++) {
+          if (inputArray1[j] !== inputArray2[j]) arraysEquivalent = false;
+        }
+      }
+
     }
   }
-  return true;
+  return arraysEquivalent;
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([[2, 3], [4]], [[2, 3], [4]]); // => true
+
+assertArraysEqual([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+assertArraysEqual([[2, 3], [4]], [[2, 3], 4]); // => false
