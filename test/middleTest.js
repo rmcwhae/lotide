ee41ad187ed1a8fafe2774beee1db42f92c96772
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-29 16:37:31 +0000

    Refactors middle to use export/import

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(middle([1]), []);
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
