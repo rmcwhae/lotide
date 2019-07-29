947cde520b32af2af3812a245b9ac9dfecc84675
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-29 16:16:21 +0000

    Refactors code to use export/import

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Lighthouse", "Lighthouse");
+
+
+module.exports = assertEqual;
