c9289ffdaeac653df37676c6448c973cf9e1e60c
Russell McWhae <russell.mcwhae@gmail.com>
2019-07-29 16:31:15 +0000

    Refactors code to use export/import

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,5 @@
-// Assertion Function
+const eqArrays = require('./eqArrays');
+
 let assertArraysEqual = function(actual, expected, message) {
   if (eqArrays(actual, expected)) {//use eqArrays function to compare arrays
     console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);//log appropriate message
@@ -7,28 +8,5 @@ let assertArraysEqual = function(actual, expected, message) {
   }
 };
 
-let eqArrays = function(inputArray1, inputArray2) {
-  let arraysEquivalent = true;
-  if (inputArray1.length !== inputArray2.length) {
-    arraysEquivalent = false;
-  } else {
-    for (let i = 0; i < inputArray1.length; i++) {
-      //if item is an array, call func recursively
-      if (Array.isArray(inputArray1[i])) {
-        arraysEquivalent = (eqArrays(inputArray1[i], inputArray2[i]));
-      } else {//continue only if item is not a sub-array (base case)
-        for (let j = 0; j < inputArray1.length; j++) {
-          if (inputArray1[j] !== inputArray2[j]) arraysEquivalent = false;
-        }
-      }
 
-    }
-  }
-  return arraysEquivalent;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([[2, 3], [4]], [[2, 3], [4]]); // => true
-
-assertArraysEqual([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
-assertArraysEqual([[2, 3], [4]], [[2, 3], 4]); // => false
+module.exports = assertArraysEqual;
